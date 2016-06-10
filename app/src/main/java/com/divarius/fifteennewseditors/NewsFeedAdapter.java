7794dfabe044633882144d2e46b74683bcc99513
Divarius <dima.skalskij@gmail.com>
2016-06-10 12:51:41 +0300

    finalize

--- app/src/main/java/com/divarius/fifteennewseditors/NewsFeedAdapter.java
+++ app/src/main/java/com/divarius/fifteennewseditors/NewsFeedAdapter.java
@@ -1,9 +1,12 @@
 package com.divarius.fifteennewseditors;
 
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,11 +20,15 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
         public ImageView image;
+        public Button btn;
+        public ImageButton icon;
 
         public ViewHolder(View v) {
             super(v);
             title = (TextView) v.findViewById(R.id.news_card_title);
             image = (ImageView) v.findViewById(R.id.news_card_img);
+            btn = (Button) v.findViewById(R.id.news_card_sub);
+            icon = (ImageButton) v.findViewById(R.id.news_card_sub_img);
         }
     }
 
@@ -41,15 +48,32 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHo
             clickListener = new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (v.getId() == R.id.news_card) {
+                    if (v.getId() == R.id.news_card_sub) {
+                        int pos = (int) v.getTag();
+                        String status = ((Button)v).getText().toString();
+                        newsArr[pos].setStatus(status.equals("Подписан") ? "unsubscribed" : "subscribed");
+                        notifyDataSetChanged();
+                    } else if (v.getId() == R.id.news_card) {
                         Toast.makeText(v.getContext(), String.valueOf(v.getTag()), Toast.LENGTH_SHORT).show();
                     }
                 }
             };
         }
 
+        String status = newsArr[position].getStatus();
+
+        if (status.equals("subscribed")) {
+            holder.icon.setImageResource(R.drawable.ok);
+            holder.btn.setText("Подписан");
+        } else {
+            holder.icon.setImageResource(R.drawable.rss);
+            holder.btn.setText("Подписаться");
+        }
+
         holder.itemView.setTag(newsArr[position].getId());
         holder.itemView.setOnClickListener(clickListener);
+        holder.btn.setOnClickListener(clickListener);
+        holder.btn.setTag(position);
         holder.title.setText(newsArr[position].getTitle());
         Picasso.with(holder.itemView.getContext())
                 .load(newsArr[position].getImageThumbnail())
